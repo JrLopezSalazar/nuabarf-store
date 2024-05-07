0bdb269bb2481bbc5a26713ded812d573f6bb3ce
JrLopezSalazar <125174691+JrLopezSalazar@users.noreply.github.com>
2024-05-07 04:03:16 -0500

    calculadora al 100 en funcionalidad

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,28 +4,51 @@ import React, { useState } from "react";
 const Calculator = () => {
     const [resultado, setResultado] = useState(0)
     const [namePet, setNamePet] = useState(null)
+    const [formData, setFormData] = useState(null);
 
 
+
+
+  // const dates = (e) => {
+  //   e.preventDefault();
+  //   const data = Object.fromEntries(new FormData(e.target));
+  //   console.log(data);
+  //   const { name, weight, age } = data;
+  //   const nameDog = (name)
+  //   setNamePet(nameDog)
+  //   const weightNumber = parseInt(weight.trim()) // Convertir weight a un número
+  //   const ageNumber = parseInt(age);
+  //   //console.log(ageNumber)
+  //   const foodAmount = calculateFood(weightNumber, ageNumber);
+  //   setResultado(foodAmount)
+  //  // console.log(foodAmount)
+  //  return foodAmount
+  // };
 
   const dates = (e) => {
     e.preventDefault();
     const data = Object.fromEntries(new FormData(e.target));
-    console.log(data);
-    const { name, weight, age } = data;
-    const nameDog = (name)
-    setNamePet(nameDog)
-    const weightNumber = parseInt(weight.trim()) // Convertir weight a un número
+    //console.log(data);
+    const { name, weight, age, activity } = data;
+    const nameDog = name;
+    setFormData(data);
+    setNamePet(nameDog);
+    const weightNumber = parseInt(weight.trim()); // Convertir weight a un número
     const ageNumber = parseInt(age);
+    const activityNumber = parseFloat(activity); // Convertir activity a un número
     //console.log(ageNumber)
-    const foodAmount = calculateFood(weightNumber, ageNumber);
-    setResultado(foodAmount)
-   // console.log(foodAmount)
-   return foodAmount
+    const foodAmount = calculateFood(weightNumber, ageNumber, activityNumber);
+    setResultado(foodAmount);
+     //console.log(ageNumber)
+    return foodAmount;
   };
+  
 
 
-  const calculateFood = (weight, age) => {
+
+  const calculateFood = (weight, age, activity) => {
     let result;
+    //console.log(age)
     
     if (age <= 4) {
       result = (weight * 10) / 100;
@@ -38,7 +61,7 @@ const Calculator = () => {
     } else if (age <= 12) {
       result = (weight * 3.5) / 100;
     } else if (age > 12) {
-      result = (weight * 3) / 100;
+      result = (weight * activity) / 100;
     } else {
       result = 0;
     }
@@ -47,7 +70,7 @@ const Calculator = () => {
   };
   
   const resultadoFormateado = resultado !== null ? resultado.toFixed(3) : ""; 
-  const porcionIndividual = resultado !== null ? (resultado / 3).toFixed(3) : "";
+  const porcionIndividual = resultado !== null ? (resultado / 4).toFixed(3) : "";
 
   return (
     <>
@@ -66,7 +89,7 @@ const Calculator = () => {
       <form className="sm:text-2xl text-xl" onSubmit={dates} >
         <section className="mb-4">
         <label className='mr-2'>Nombre de su mascota</label>
-      <input className='border w-40 ml-2' type="text" id="name" name="name" />
+      <input className='border w-40 ml-2 capitalize' type="text" id="name" name="name" />
         </section>
        
         <section className="mb-4">
@@ -89,12 +112,27 @@ const Calculator = () => {
           <input className="border-4 ml-2 w-40" type="number" name="weight" id="weight"  />
         </section>
 
+        <section className="mb-4">
+          <label className="mb-4" htmlFor="numero">Nivel de actividad:</label>
+          <select className="ml-2 border" name="activity" id="activity">
+            <option value="2.8">Normal</option>
+            <option value="3.2">Media</option>
+            <option value="4.2">Alta</option>
+        
+          </select>
+        </section>
+
         <button className="px-4 py-2 rounded-md text-white font-medium bg-gradient-to-r bg-orange-600  mb-8 " type="submit">Calcular</button>
       </form>
       <div className="text-2xl sm:text-xl mb-6">
-      <h3 className="t">
-  Ración diaria para {namePet} {resultadoFormateado} kilogramos por día, dividido en 3 porciones de {porcionIndividual} kilogramos cada una.
+     
+
+
+      <h3 className="">
+  Ración diaria para <span className="capitalize"> {namePet}</span> es {resultadoFormateado} kilogramos por día, dividido en {formData && formData.age < 14 ? "4" : "2 "} porciones de {formData && formData.age < 14 ? porcionIndividual : (resultado / 2).toFixed(3)} kilogramos cada una.
 </h3>
+
+
       </div>
     </section>
       
@@ -108,3 +146,4 @@ const Calculator = () => {
 };
 
 export default Calculator;
+
